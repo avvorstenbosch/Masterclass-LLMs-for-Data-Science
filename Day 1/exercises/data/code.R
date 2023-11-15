3600cb808f442a2a751dceee09021e16e35948f2
avvorstenbosch <alex_van_vorstenbosch@hotmail.com>
2023-11-15 14:10:32 +0100

    Finished exercises pair-programming

--- Day 1/exercises/data/code.R
+++ Day 1/exercises/data/code.R
@@ -1,20 +1,28 @@
-data <- readLines("data-puzzle.txt")
-numbers <- as.integer(unlist(strsplit(data[1], ",")))
 
-boards <- data[-(1:2)]
-boards <- boards[boards != ""]
-board_list <- split(boards, ceiling(seq_along(boards)/5))
-board_list <- lapply(board_list, function(board_lines) {
-  # Split each line into numbers, handling extra spaces
-  board_numbers <- unlist(lapply(board_lines, function(line) {
-    as.integer(unlist(strsplit(trimws(line), "\\s+")))
-  }))
-  # Ensure that each board is a 5x5 matrix
-  if (length(board_numbers) != 25) {
-    stop("Error: Board size is not 5x5.")
-  }
-  matrix(board_numbers, nrow = 5, byrow = TRUE)
-})
+read_data <- function(){
+  data <- readLines("input-data-script.txt")
+  numbers <- as.integer(unlist(strsplit(data[1], ",")))
+
+  boards <- data[-(1:2)]
+  boards <- boards[boards != ""]
+  board_list <- split(boards, ceiling(seq_along(boards)/5))
+  board_list <- lapply(board_list, function(board_lines) {
+    # Split each line into numbers, handling extra spaces
+    board_numbers <- unlist(lapply(board_lines, function(line) {
+      as.integer(unlist(strsplit(trimws(line), "\\s+")))
+    }))
+    # Ensure that each board is a 5x5 matrix
+    if (length(board_numbers) != 25) {
+      stop("Error: Board size is not 5x5.")
+    }
+    matrix(board_numbers, nrow = 5, byrow = TRUE)
+  })
+  returned_data <- list("numbers" = numbers, "board_list"=board_list)
+  return(returned_data)
+}
+results_read <- read_data()
+numbers <- results_read$numbers
+board_list <- results_read$board_list
 
 boards_filled <- lapply(board_list, function(x) matrix(0, nrow = 5, ncol = 5))
 
@@ -27,10 +35,15 @@ check_winner <- function(boards_filled) {
   return(NULL)
 }
 
-for (num in numbers) {
+mark_number <- function(num){
   for (i in seq_along(board_list)) {
     boards_filled[[i]][board_list[[i]] == num] <- 1
   }
+  return(boards_filled)
+}
+
+for (num in numbers) {
+  boards_filled <- mark_number(num)
   winner <- check_winner(boards_filled)
   if (!is.null(winner)) {
     break
--- Day 1/exercises/data/code.py
+++ Day 1/exercises/data/code.py
@@ -1,22 +1,30 @@
 import numpy as np
 import re
 
-with open('data-puzzle.txt', 'r') as f:
-    data = f.read().strip().split('\n')
-    numbers = [int(num) for num in re.split(',', data[0].strip())]
-    boards = []
-    board = []
-    for i in range(2, len(data)):
-        if not data[i]:
-            boards.append(board)
-            board = []
-        else:
-            board.append([int(num) for num in re.split('\\s+', data[i].strip())])
-    boards=np.array(boards)
+def read_data():
+    with open('input-data-script.txt', 'r') as f:
+        data = f.read().strip().split('\n')
+        numbers = [int(num) for num in re.split(',', data[0].strip())]
+        boards = []
+        board = []
+        for i in range(2, len(data)):
+            if not data[i]:
+                boards.append(board)
+                board = []
+            else:
+                board.append([int(num) for num in re.split('\\s+', data[i].strip())])
+        boards=np.array(boards)
+    return numbers, boards
 
+numbers, boards = read_data()
 boards_filled = np.zeros_like(boards)
 
-def return_winner():
+def mark_number(num):
+    num = int(num)
+    mask = (boards == num)
+    boards_filled[mask] = 1
+
+def return_winner(boards_filled=boards_filled):
     vert = (boards_filled.sum(axis=1)==5)
     hor = (boards_filled.sum(axis=2)==5)
     if np.any(vert):
@@ -29,12 +37,10 @@ def return_winner():
     return winner
 
 for num in numbers:
-    int(num)
-    mask = (boards == num)
-    boards_filled[mask] = 1
+    mark_number(num)
     vert = (boards_filled.sum(axis=1)==5)
     hor = (boards_filled.sum(axis=2)==5)
-    winner = return_winner()
+    winner = return_winner(boards_filled)
     if winner is not None:
         break
 
@@ -45,4 +51,4 @@ else:
     
 unmarked = (boards_filled[winner]==0)
 answer = boards[winner][unmarked].sum()*num
-print(f"The winner is board {winner} with a score of {answer}!")
+print(f"The winner is board {winner+1} with a score of {answer}!")
