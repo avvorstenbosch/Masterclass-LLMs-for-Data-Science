4165202747aefe36fe1a9f38031b0b1dd7404296
avvorstenbosch <alex_van_vorstenbosch@hotmail.com>
2023-11-19 22:57:15 +0100

    styling code

--- Day 2/exercises/utils/chat_completions.R
+++ Day 2/exercises/utils/chat_completions.R
@@ -2,77 +2,77 @@
 #-------------------------------------------------------------------------------
 
 value_between <- function(x, lower, upper) {
-    x >= lower && x <= upper
+  x >= lower && x <= upper
 }
 
 assertthat::on_failure(value_between) <- function(call, env) {
-    paste0(
-        deparse(call$x),
-        " is not between ",
-        deparse(call$lower),
-        " and ",
-        deparse(call$upper)
-    )
+  paste0(
+    deparse(call$x),
+    " is not between ",
+    deparse(call$lower),
+    " and ",
+    deparse(call$upper)
+  )
 }
 
 #-------------------------------------------------------------------------------
 
 both_specified <- function(x, y) {
-    x != 1 && y != 1
+  x != 1 && y != 1
 }
 
 #-------------------------------------------------------------------------------
 
 length_between <- function(x, lower, upper) {
-    length(x) >= lower && length(x) <= upper
+  length(x) >= lower && length(x) <= upper
 }
 
 assertthat::on_failure(length_between) <- function(call, env) {
-    paste0(
-        "Length of ",
-        deparse(call$x),
-        " is not between ",
-        deparse(call$lower),
-        " and ",
-        deparse(call$upper)
-    )
+  paste0(
+    "Length of ",
+    deparse(call$x),
+    " is not between ",
+    deparse(call$lower),
+    " and ",
+    deparse(call$upper)
+  )
 }
 
 #-------------------------------------------------------------------------------
 
 n_characters_between <- function(x, lower, upper) {
-    nchar(x) >= lower && nchar(x) <= upper
+  nchar(x) >= lower && nchar(x) <= upper
 }
 
 assertthat::on_failure(n_characters_between) <- function(call, env) {
-    paste0(
-        "Number of characters of ",
-        deparse(call$x),
-        " is not between ",
-        deparse(call$lower),
-        " and ",
-        deparse(call$upper)
-    )
+  paste0(
+    "Number of characters of ",
+    deparse(call$x),
+    " is not between ",
+    deparse(call$lower),
+    " and ",
+    deparse(call$upper)
+  )
 }
 
 #-------------------------------------------------------------------------------
 
 is_false <- function(x) {
-    !x
+  !x
 }
 
 assertthat::on_failure(is_false) <- function(call, env) {
-    paste0(deparse(call$x), " is not yet implemented.")
+  paste0(deparse(call$x), " is not yet implemented.")
 }
 
 #-------------------------------------------------------------------------------
 
 is_null <- function(x) {
-    is.null(x)
+  is.null(x)
 }
 
 assertthat::on_failure(is_null) <- function(call, env) {
-    paste0(deparse(call$x), " is not yet implemented.")
+  paste0(deparse(call$x), " is not yet implemented.")
 }
 
 create_chat_completion <- function(model,
--- Day 2/exercises/utils/get_embedding.R
+++ Day 2/exercises/utils/get_embedding.R
@@ -1,6 +1,5 @@
-get_embedding <- function(string=NULL,
-                          embedding_model="text-embedding-ada-002"
-                          ){
+get_embedding <- function(string = NULL,
+                          embedding_model = "text-embedding-ada-002") {
   #' Flexible API Request Function with Integrated Glue Functionality
   #'
   #' This function sends a request to a specified API, integrating the 'glue' function to allow
@@ -11,11 +10,11 @@ get_embedding <- function(string=NULL,
   #'
   #' @return
   #' The response object from the chat_completion api
-    response <- create_embedding(
-                model = embedding_model,
-                input = string,
-                openai_api_key = OPENAI_API_KEY
-                )
-    embedding <- as.vector(response$data$embedding[[1]])
-    return(embedding)
+  response <- create_embedding(
+    model = embedding_model,
+    input = string,
+    openai_api_key = OPENAI_API_KEY
+  )
+  embedding <- as.vector(response$data$embedding[[1]])
+  return(embedding)
 }
